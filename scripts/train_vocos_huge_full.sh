496f708bd043705bfddc496b9e75d0c40127da99
Lengyue <lengyue@lengyue.me>
2023-08-02 15:43:38 -0400

    update configs

---
+++ scripts/train_vocos_huge_full.sh
@@ -0,0 +1,8 @@
+python fish_vocoder/train.py task_name=vocos-huge-full \
+    model/generator=vocos-huge \
+    model.num_mels=128 \
+    model.mel_transforms.modules.input.f_min=0 \
+    model.mel_transforms.modules.input.f_max=22050 \
+    data.batch_size=4 \
+    trainer.precision=16-mixed \
+    logger=tensorboard
