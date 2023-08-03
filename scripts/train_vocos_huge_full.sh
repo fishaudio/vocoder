5d9d67233486f1a311d096037f99b60f9c15a677
Lengyue <lengyue@lengyue.me>
2023-08-02 20:46:29 -0400

    update n mels

--- scripts/train_vocos_huge_full.sh
+++ scripts/train_vocos_huge_full.sh
@@ -1,6 +1,6 @@
 python fish_vocoder/train.py task_name=vocos-huge-full \
     model/generator=vocos-huge \
-    model.num_mels=128 \
+    model.num_mels=160 \
     model.mel_transforms.modules.input.f_min=0 \
     model.mel_transforms.modules.input.f_max=22050 \
     data.batch_size=4 \
