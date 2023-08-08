da45439348e0eb63bfbdadf1474f3f50a4a60cc1
Lengyue <lengyue@lengyue.me>
2023-08-08 01:27:19 -0400

    Update batch size

--- scripts/train_convnext_bigvgan_base.sh
+++ scripts/train_convnext_bigvgan_base.sh
@@ -1,4 +1,5 @@
 python fish_vocoder/train.py task_name=convnext-bigvgan-base \
     model/generator=convnext-bigvgan-base \
     model.num_frames=16 \
+    data.batch_size=32 \
     logger=tensorboard
